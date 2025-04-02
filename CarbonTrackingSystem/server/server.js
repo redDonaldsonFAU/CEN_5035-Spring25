9e9e635dc8d57930c6bae57ac0e08a37f27a760f
Red Donaldson <donaldsond23@fau.edu>
2025-04-02 17:46:10 -0400

    Further updates

--- CarbonTrackingSystem/server/server.js
+++ CarbonTrackingSystem/server/server.js
@@ -1,23 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const PORT = 3000;
-import path from "path";
+const path = require("path");
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 app.get('*', (request, response) => {
-	const status = {
-		Status: "Running"
-	};
+  const status = {
+    Status: "Running"
+  };
 
-	response.send(status);;
+  response.send(status);;
 });
 
 app.listen(PORT, () => {
-	console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
 
 app.post("/signup", (request, response) => {
-	console.log(JSON.stringify(request));
+  console.log(JSON.stringify(request));
 
-	response.send('OK');
+  response.send('OK');
 });
