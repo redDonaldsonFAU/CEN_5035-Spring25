7c90f1500ff07cf0f147d8c3ed29394709c1bf44
Red Donaldson <donaldsond23@fau.edu>
2025-04-05 17:03:19 -0400

    Adding in a custom component for an image carousel

--- CarbonTrackingSystem/src/app/pages/home/home.component.ts
+++ CarbonTrackingSystem/src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [NgOptimizedImage],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
