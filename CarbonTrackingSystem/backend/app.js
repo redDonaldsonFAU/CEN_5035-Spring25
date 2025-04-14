ce952c67b451d4340a206aa83d939f996dc60e3b
bsanford2016 <bsanford2016@fau.edu>
2025-04-14 02:05:51 -0400

    updated backend

---
+++ CarbonTrackingSystem/backend/app.js
@@ -0,0 +1,54 @@
+require('dotenv').config({ path: './src/.env' });
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const https = require('https');
+
+const app = express();
+// const PORT = 3000;
+// check port 10260
+const PORT = 10260;
+
+// enable cors
+app.use(cors());
+
+// enable json parser
+app.use(express.json());
+
+// route the trip api
+const tripRoutes = require('./routes/trips_com');
+
+// use the route
+app.use('/api/trips', tripRoutes);
+
+// route string
+app.get('/', (req, res) => {
+    res.send("Welcome to Trip API !")
+})
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server is successfully listening at port:", PORT);
+    else
+        console.error('An error occurred:', error);
+})
+
+// ### http trip URL ###
+const tripURL = ''
+
+//call function whenever application is used:
+main().catch((error) => console.error(error));
+
+//connecting to mongoose (MongoDB)
+async function main() {
+    //prepare connection string
+    const connectionString = process.env.MONGODB_URI;
+    //error handling
+    console.log("Connection String:", connectionString);
+    await mongoose.connect(connectionString);
+    mongoose.set("strictQuery", true);
+
+}
+
+//export
+module.exports = {app};
